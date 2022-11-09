252d878e6932cdbb5481a519b0401a445ee363a0
lmx <854356783@qq.com>
2022-11-09 10:04:54 +0800

    调整

--- cmd/root.go
+++ cmd/root.go
@@ -2,20 +2,19 @@ package cmd
 
 import (
 	"FilesMap/util"
-	"encoding/json"
 	"fmt"
-	"github.com/spf13/cobra"
 	"io/ioutil"
 	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "FilesMap",
 	Short: "重命名文件夹文件并生成前后对照表",
-	Long: `重命名文件夹文件并生成前后对照表,运行传入文件夹路径即可`,
+	Long:  `重命名文件夹文件并生成前后对照表,运行传入文件夹路径即可`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
@@ -43,26 +42,23 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-func generateFilesMap(path string) map[string]interface{} {
+func generateFilesMap(path string) map[string]string {
 	files := util.AllSubPath(path)
 	if files == nil {
 		return nil
 	}
 
-	filesMap := make(map[string]interface{})
+	filesMap := make(map[string]string)
 
 	for i, filePath := range files {
 		oldName := util.FileFullName(filePath)
-		if oldName == ".DS_Store" {
+		if strings.Contains(oldName, "DS_Store") {
 			continue
 		}
-		if util.IsDir(filePath) {
-			subFilesMap := generateFilesMap(filePath)
-			filesMap[oldName] = subFilesMap
-		}else {
-			_ ,suffixName := util.FileName(filePath)
-			newName := fmt.Sprintf("%d%s",i, suffixName)
-			err01 := os.Rename(filePath, path+ "/" + newName)
+		if !util.IsDir(filePath) {
+			_, suffixName := util.FileName(filePath)
+			newName := fmt.Sprintf("%d%s", i, suffixName)
+			err01 := os.Rename(filePath, path+"/"+newName)
 			if err01 != nil {
 				fmt.Println("重命名出错:", oldName)
 				continue
@@ -73,20 +69,29 @@ func generateFilesMap(path string) map[string]interface{} {
 	return filesMap
 }
 
-func saveFilesMap(path string, filesMap map[string]interface{})  {
-	js, err := json.Marshal(filesMap)
-	if err != nil {
-		fmt.Println("转json出错",filesMap)
-		return
+func saveFilesMap(path string, filesMap map[string]string) {
+	str := ""
+	for key, value := range filesMap {
+		str = str + key + "--" + value + "\n"
 	}
-	
-	jsonPath := path + "/filesMap.json"
+	textPath := path + "/filesMap.text"
 
-	err02 := ioutil.WriteFile(jsonPath, js, os.ModePerm)
-	if err02 != nil {
-		fmt.Println("写入json错误", jsonPath)
+	err := ioutil.WriteFile(textPath, []byte(str), os.ModePerm)
+	if err != nil {
+		fmt.Println("写入错误", textPath)
 	}
+
+	// js, err := json.Marshal(filesMap)
+	// if err != nil {
+	// 	fmt.Println("转json出错", filesMap)
+	// 	return
+	// }
+
+	// jsonPath := path + "/filesMap.json"
+
+	// err02 := ioutil.WriteFile(jsonPath, js, os.ModePerm)
+	// if err02 != nil {
+	// 	fmt.Println("写入json错误", jsonPath)
+	// }
 
 }
-
-
